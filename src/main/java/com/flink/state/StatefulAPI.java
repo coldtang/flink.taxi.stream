eedd3985935d6855dd1fdecba9dc64a5b8d441c1
唐善群 <tsq49510@ly.com>
2020-10-27 00:55:54 +0800

    ValueState

---
+++ src/main/java/com/flink/state/StatefulAPI.java
@@ -0,0 +1,29 @@
+package com.flink.state;
+
+import com.flink.state.function.CountWindowAverageWithValueState;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * 需求：当接收到的相同 key 的元素个数等于 3 个或者超过 3 个的时候
+ *  就计算这些元素的 value 的平均值。
+ *  计算 keyed stream 中每 3 个元素的 value 的平均值
+ */
+public class StatefulAPI {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<Tuple2<Long, Long>> dataStreamSource =
+                env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L),
+                        Tuple2.of(2L, 4L), Tuple2.of(2L, 2L), Tuple2.of(2L, 5L));
+
+/*        dataStreamSource
+                .keyBy(0)
+                .flatMap(new CountWindowAverageWithValueState())
+                .print();*/
+
+
+        env.execute("TestStatefulApi");
+    }
+}
